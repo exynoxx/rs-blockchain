6784d3a222e31a430747015081956eed1543c9e9
Nicholas <nicho.tjornelund@gmail.com>
2021-07-01 15:52:54 +0200

    crypto stash not working

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,22 +1,35 @@
-use crypto::ed25519::{keypair, signature, verify as crypto_verify};
-use crypto::sha3::Sha3;
-use rand::Rng;
+use rand_core::OsRng;
+use elgamal_ristretto::public::PublicKey;
+use elgamal_ristretto::private::SecretKey;
 
-pub fn gen() -> ([u8; 32], [u8; 64]) {
-    println!("a");
-    let random_bytes = [1u8, 2u8, 3u8]; //rand::thread_rng().gen::<[u8; 32]>();
-    let (sk, pk) = keypair(&random_bytes);
+extern crate rand;
+extern crate elgamal_ristretto;
+use curve25519_dalek_fiat::ristretto::RistrettoPoint;
+use curve25519_dalek_fiat::scalar::Scalar;
+
+
+pub fn gen() -> (PublicKey, SecretKey) {
+    let mut csprng = OsRng;
+    // Generate key pair
+    let sk = SecretKey::new(&mut csprng);
+    let pk = PublicKey::from(&sk);
     return (pk, sk);
 }
 
-pub fn sign(msg: &[u8], sk: &[u8]) -> [u8; 64] {
-    println!("b");
-    return signature(msg, sk);
+pub fn encode(msg: &[u8]) -> RistrettoPoint{
+    return RistrettoPoint::hash_from_bytes::<sha3>(msg);
 }
 
-pub fn verify(msg: &[u8], pk: &[u8], signature: &[u8]) -> bool {
+pub fn sign(msg: &[u8], sk: &SecretKey) -> (Scalar, RistrettoPoint) {
+    println!("b");
+    let m = encode(msg);
+    return sk.sign(&m);
+}
+
+pub fn verify(msg: &[u8], pk: &PublicKey, signature: &(Scalar, RistrettoPoint)) -> bool {
     println!("c");
-    return crypto_verify(msg, pk, signature);
+    let m = encode(msg);
+    return pk.verify_signature(&m, signature);
 }
 
 pub fn sha3(msg: &[u8]) -> [u8; 64] {
