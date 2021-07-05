7986363435bf9cbd0754e147e7eff8463ef9674c
Nicholas <nicholas@XPS>
2021-07-05 17:04:46 +0200

    serialize

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,7 +1,9 @@
-use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme};
+use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme, BigUint, PublicKeyEncoding};
 use rand::rngs::OsRng;
 use sha2::{Sha256, Digest};
 use rsa::hash::Hash::SHA2_256;
+use serde::{Serialize, Deserialize};
+use bincode::{deserialize, serialize};
 
 pub fn test() {
     let mut rng = OsRng;
@@ -43,4 +45,22 @@ pub fn sha256(data: &[u8]) -> Vec<u8> {
     let mut hasher = Sha256::new();
     hasher.update(data);
     return hasher.finalize().to_vec();
+}
+
+pub fn serialize_key(pk: &RSAPublicKey) -> Vec<u8>{
+    return pk.to_pkcs8().expect("could not serialize PK")
+}
+
+pub fn deserialize_key(pk: &Vec<u8>) -> RSAPublicKey{
+    return RSAPublicKey::from_pkcs8(&pk).expect("could not reconstruct Pk from [u8]")
+}
+
+pub fn key_to_string(pk: &RSAPublicKey) -> String {
+    let s = serialize_key(pk);
+    return hex::encode(s);
+}
+
+pub fn string_to_key(s: &String) -> RSAPublicKey {
+    let v = hex::decode(s).expect("error wrong hex encoding");
+    return deserialize_key(&v);
 }
